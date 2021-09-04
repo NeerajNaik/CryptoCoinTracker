87e63ef36e9576872e2f6f63a23148f4b42ecf33
NeerajNaik <neerajnaik125@gmail.com>
2021-09-04 20:27:07 +0530

    added CryptoTracker project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect,useState } from 'react';
+import axios from 'axios';
+import Coin from './components/coinItems/Coin';
+
 
 function App() {
+  const [coins, setcoins] = useState([])
+  const [search, setsearch] = useState("")
+  const [dropDown, setdropDown] = useState("50")
+  useEffect(() => {
+    axios
+      .get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${dropDown}&page=1&sparkline=false`)
+      .then((res)=>{
+        setcoins(res.data)
+        console.log(res.data)})
+      .catch((err)=>console.error(err))
+      console.log(dropDown)
+  }, [dropDown])
+
+  const handleChange=(e)=>{
+    setsearch(e.target.value)
+  }
+
+  const handleDropDown=(limit)=>{
+    setdropDown(limit)
+    // document.getElementById("dd").inne
+  }
+
+  const filteredCoins = coins.filter((coin)=>coin.name.toLowerCase().includes(search.toLowerCase()))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+      <h1 className="brand"><i className="fas fa-moon headerRender"></i>CoinMoon</h1>
+      <form>
+        <input className="inputField" type="text" onChange={handleChange} placeholder="Search Coin"/>
+        <select name="dd" id="dd"  defaultValue={dropDown} onChange={(e)=>handleDropDown(e.target.value)}>
+          <option value="10">Current Limit: 10 </option>
+          <option value="50">Current Limit: 50 </option>
+          <option value="100">Current Limit: 100 </option>
+        </select>
+      </form>
+    </div>
+    <div className="coinsContainer">
+    {
+      filteredCoins.map((coin)=>
+      {
+       return <Coin
+                key={coin.id}
+                name={coin.name}
+                price={coin.current_price}
+                symbol={coin.symbol}
+                marketcap={coin.market_cap}
+                volume={coin.total_volume}
+                image={coin.image}
+                priceChange={coin.price_change_percentage_24h}
+              />
+      })
+    }
+    </div>
     </div>
   );
 }
